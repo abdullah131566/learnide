74c86b1e11f6934626cfe1cd46c1fbcf7c3bce1a
abdullah131566 <107644072+abdullah131566@users.noreply.github.com>
2023-12-19 16:58:47 +0500

    Move layout code from home to learnide route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,5 @@
 <script>
   import '../global.css';
-  import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<AppShell>
-  <svelte:fragment slot="header">
-    <header class="border border-red-400">Header</header>
-  </svelte:fragment>
-  <svelte:fragment slot="sidebarLeft">
-    <aside class="border border-blue-400 w-full h-full">Sidebar Left</aside>
-  </svelte:fragment>
-  <!-- (sidebarRight) -->
-  <svelte:fragment slot="pageHeader">
-    <header class="border border-purple-400">Page Header</header>
-  </svelte:fragment>
-  <!-- Router Slot -->
-  <slot>
-    <main class="border border-yellow-400 w-full h-full">Page Content</main>
-  </slot>
-  <!-- ---- / ---- -->
-  <svelte:fragment slot="pageFooter">
-    <footer class="border border-green-400">Page Footer</footer>
-  </svelte:fragment>
-  <!-- (footer) -->
-</AppShell>
+<slot />
---
+++ src/routes/learnide/+layout.svelte
@@ -0,0 +1,56 @@
+<script>
+  import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+  import LearnideLogo from '../../assets/learnide-logo.svg';
+  import LearnideTitle from '../../assets/learnide-title.svg';
+  import LearnideTitleLight from '../../assets/learnide-title-light.svg';
+
+  const emailInitials = 'AB';
+  let isDarkMode = true;
+
+  $: titleImage = isDarkMode ? LearnideTitleLight : LearnideTitle;
+</script>
+
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar
+      background="bg-surface-50-900-token"
+      padding="px-4 py-2"
+      border="border-b-2 border-surface-300-600-token"
+    >
+      <svelte:fragment slot="lead">
+        <div class="h-5/6 flex items-center gap-2">
+          <img src={LearnideLogo} class="h-6" alt="The Learnide Logo" />
+          <img src={titleImage} class="h-4" alt="The Learnide Title" /></div
+        >
+      </svelte:fragment>
+      <svelte:fragment slot="trail">
+        <Avatar
+          initials={emailInitials}
+          width="w-10"
+          cursor="cursor-pointer"
+          shadow="hover:shadow-md"
+          fill="fill-primary-900"
+          border="transition duration-500 border-2 border-primary-700-200-token hover:border-primary-600-300-token"
+          background="bg-primary-400-500-token"
+        />
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+    <aside class="border-r-2 border-surface-300-600-token px-4 h-full w-80"
+      >Sidebar Left alkw rweol rhwoe htrow rhow hoewr htow h Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Harum, fugiat.t</aside
+    >
+  </svelte:fragment>
+  <!-- (sidebarRight) -->
+  <svelte:fragment slot="pageHeader">
+    <header class="border-b-2 border-surface-300-600-token">Page Header</header>
+  </svelte:fragment>
+  <!-- Router Slot -->
+  <slot />
+  <!-- ---- / ---- -->
+  <svelte:fragment slot="pageFooter">
+    <footer class="border-t-2 border-surface-300-600-token">Page Footer</footer>
+  </svelte:fragment>
+  <!-- (footer) -->
+</AppShell>
